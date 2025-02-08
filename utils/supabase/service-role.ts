ad64be484495b9498e60e0c85a05f6429e883dc0
Elias Landin <elias.landin@me.com>
2025-02-08 16:15:09 +0100

    Add supabase service role client

---
+++ utils/supabase/service-role.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { createClient } from "@supabase/supabase-js";
+
+export const createServiceRoleClient = async () => {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_SECRET!,
+    {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+        detectSessionInUrl: false,
+      },
+    },
+  );
+};
