9b911583915f5bae7a1b54806b52ce8a16dc8d0f
Elias Landin <elias.landin@me.com>
2025-02-09 12:09:25 +0100

    Add capitalize fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalize(string: string) {
+  const words = string.split(" ");
+  const capWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1),
+  );
+
+  return capWords.join(" ");
 }
