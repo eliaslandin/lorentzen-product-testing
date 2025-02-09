76111c78809c86728fd56fc5e6efe06635ac04b7
Elias Landin <elias.landin@me.com>
2025-02-09 17:46:38 +0100

    Fix current user check

--- app/admin/actions.ts
+++ app/admin/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { createClient } from "@/utils/supabase/server";
 import { createServiceRoleClient } from "@/utils/supabase/service-role";
 import { AuthError, User } from "@supabase/supabase-js";
 import { revalidatePath } from "next/cache";
@@ -28,7 +29,7 @@ export const createUserAction = async (
   | ServerSuccessResponse<{ user: User }>
   | ServerErrorResponse<AuthError | CustomError>
 > => {
-  const name = formData.get("name");
+  const name = formData.get("name") as string;
 
   if (!name) {
     return {
@@ -40,21 +41,23 @@ export const createUserAction = async (
     };
   }
 
-  const supabase = await createServiceRoleClient();
-
+  const supabase = await createClient();
   const { data: getUserData } = await supabase.auth.getUser();
 
   if (
     !getUserData.user ||
     getUserData.user.id !== "c3936b27-ada1-4f72-b3cc-8db2ab18fe5b"
   ) {
-    console.error(`Unauthorized. Get user response: ${getUserData}`);
+    console.error(
+      `Unauthorized. Get user response: ${JSON.stringify(getUserData)}`,
+    );
     throw new Error("Unauthorized.");
   }
 
   console.log("Invoking user: ", getUserData.user);
 
-  const { data, error } = await supabase.auth.admin.createUser({
+  const supabaseAdmin = await createServiceRoleClient();
+  const { data, error } = await supabaseAdmin.auth.admin.createUser({
     email: `tp-${crypto.randomUUID()}@lenalorentzendesign.se`,
     email_confirm: true,
     user_metadata: {
@@ -76,3 +79,56 @@ export const createUserAction = async (
     data: data,
   };
 };
+/* 
+export const createTestAction = async (
+  prevState: any,
+  formData: FormData,
+): Promise<
+  | ServerSuccessResponse<{ user: User }>
+  | ServerErrorResponse<AuthError | CustomError>
+> => {
+  const title = formData.get("title") as string;
+
+  if (!title) {
+    return {
+      isError: true,
+      error: {
+        isCustom: true,
+        message: "Input a title",
+      },
+    };
+  }
+
+  const supabase = await createServiceRoleClient();
+
+  const { data: getUserData } = await supabase.auth.getUser();
+
+  if (
+    !getUserData.user ||
+    getUserData.user.id !== "c3936b27-ada1-4f72-b3cc-8db2ab18fe5b"
+  ) {
+    console.error(`Unauthorized. Get user response: ${getUserData}`);
+    throw new Error("Unauthorized.");
+  }
+
+  console.log("Invoking user: ", getUserData.user);
+
+  const { data } = await supabase.from("test").insert({
+    title,
+  });
+
+  if (error) {
+    return {
+      isError: true,
+      error,
+    };
+  }
+
+  revalidatePath("/admin/test-persons");
+
+  return {
+    isSuccess: true,
+    data: data,
+  };
+};
+ */
