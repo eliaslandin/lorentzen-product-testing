a6945c964b480ca9a59022aac80dc0d13d4d944d
Elias Landin <elias.landin@me.com>
2025-02-10 18:16:59 +0100

    Decrease size of card and center on page

--- app/admin/test-persons/add/page.tsx
+++ app/admin/test-persons/add/page.tsx
@@ -3,8 +3,10 @@ import { CreateTestPersonForm } from "@/components/create-test-person-form";
 
 export default function AddUserPage() {
   return (
-    <View>
-      <CreateTestPersonForm />
+    <View className="items-center">
+      <View className="max-w-2xl">
+        <CreateTestPersonForm />
+      </View>
     </View>
   );
 }
