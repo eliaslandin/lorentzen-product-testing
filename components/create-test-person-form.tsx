b0447e4ba56f725c2c03e57f23648830fd3856d1
Elias Landin <elias.landin@me.com>
2025-02-15 22:27:02 +0100

    Add personal number input field

--- components/create-test-person-form.tsx
+++ components/create-test-person-form.tsx
@@ -10,6 +10,8 @@ import { FormContent } from "./form-content";
 import { FormField } from "./form-field";
 import { FormErrorMessage } from "./form-error-message";
 import { FormSubmitButton } from "./form-submit-button";
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "./ui/input-otp";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
 
 export const CreateTestPersonForm = () => {
   const [lastResult, formAction, pending] = useActionState(
@@ -25,7 +27,7 @@ export const CreateTestPersonForm = () => {
       });
     },
     shouldRevalidate: "onInput",
-    shouldValidate: "onInput",
+    shouldValidate: "onBlur",
   });
 
   return (
@@ -42,6 +44,44 @@ export const CreateTestPersonForm = () => {
             name={fields.name.name}
             defaultValue={fields.name.initialValue}
           />
+        </FormField>
+        <FormField
+          label="Personnummer (YYYY-MM-DD-NNNN)"
+          inputId={fields.personal_number.id}
+          errorMessage={fields.personal_number.errors}
+        >
+          <InputOTP
+            id={fields.personal_number.id}
+            key={fields.personal_number.key}
+            name={fields.personal_number.name}
+            defaultValue={fields.personal_number.initialValue}
+            maxLength={12}
+            pattern={REGEXP_ONLY_DIGITS}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+              <InputOTPSlot index={3} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={6} />
+              <InputOTPSlot index={7} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={8} />
+              <InputOTPSlot index={9} />
+              <InputOTPSlot index={10} />
+              <InputOTPSlot index={11} />
+            </InputOTPGroup>
+          </InputOTP>
         </FormField>
         <FormSubmitButton pending={pending}>Skapa</FormSubmitButton>
         <FormErrorMessage>{form.errors}</FormErrorMessage>
--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 
 export const nonEmptyStringSchema = z.string({
-  required_error: "Måste fyllas i",
+        required_error: "Måste fyllas i",
 });
 
 export const stringMax255Schema = nonEmptyStringSchema.max(
-  255,
-  "Texten får vara max 255 tecken lång",
+        255,
+        "Texten får vara max 255 tecken lång",
 );
 
 export const createTestPersonSchema = z.object({
-  name: stringMax255Schema,
+        name: stringMax255Schema,
+        personal_number: z.number().min(190000000000, "Ogiltigt personnummer").max(999999999999, "Ogiltigt personnummber"),
 });
