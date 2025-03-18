510c2ff10b5d318f17567b944a685884bd435ebc
Elias Landin <elias.landin@me.com>
2025-03-18 09:41:01 +0100

    Make test items links and add city name to items

--- app/admin/tester/page.tsx
+++ app/admin/tester/page.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { getTests } from "@/lib/fetchers";
+import Link from "next/link";
 
 export default async function Page() {
   const { data, error } = await getTests();
@@ -18,9 +19,15 @@ export default async function Page() {
       <CardContent>
         <ul className="flex flex-col gap-3">
           {data.map((test) => (
-            <li key={test.id} className="border rounded-md p-4 bg-background">
-              {test.name}
-            </li>
+            <Link href={`/admin/tester/${test.id}`}>
+              <li
+                key={test.id}
+                className="flex w-full justify-between border rounded-md p-4 bg-background"
+              >
+                <p>{test.name}</p>
+                <p>{test.city_name}</p>
+              </li>
+            </Link>
           ))}
         </ul>
       </CardContent>
