b65f07395461814bdcda59f987d9d8c2e2b983d2
Elias Landin <elias.landin@me.com>
2025-03-21 20:35:03 +0100

    Add basic code for participant section

--- app/admin/tester/[id]/page.tsx
+++ app/admin/tester/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { LoadingCard } from "@/components/loading-card";
+import { ParticipantsSection } from "@/components/participants-section";
 import { TestInfoSection } from "@/components/test-info-section";
 import { View } from "@/components/view";
 import { Suspense } from "react";
@@ -11,10 +12,15 @@ export default async function Page({
   const { id } = await params;
 
   return (
-    <View>
+    <View className="gap-4">
       <Suspense fallback={<LoadingCard />}>
         <TestInfoSection id={id} />
       </Suspense>
+      <View>
+        <Suspense fallback={<LoadingCard />}>
+          <ParticipantsSection />
+        </Suspense>
+      </View>
     </View>
   );
 }
---
+++ components/participants-section.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { H2 } from "./H2";
+
+export const ParticipantsSection = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <H2>Testpersoner</H2>
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
