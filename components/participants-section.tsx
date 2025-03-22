59b9e14c8bafcca6926d369dc879af412fa12d9b
Elias Landin <elias.landin@me.com>
2025-03-22 15:51:11 +0100

    Add tabs to card in participant section

--- components/participants-section.tsx
+++ components/participants-section.tsx
@@ -3,6 +3,7 @@ import { H2 } from "./H2";
 import { LoadingCard } from "./loading-card";
 import { Suspense } from "react";
 import { View } from "./view";
+import { Tabs, TabsTrigger, TabsList, TabsContent } from "./ui/tabs";
 
 export const ParticipantsSection = () => {
   return (
@@ -10,8 +11,18 @@ export const ParticipantsSection = () => {
       <H2>Testpersoner</H2>
       <Suspense fallback={<LoadingCard />}>
         <Card>
-          <CardHeader></CardHeader>
-          <CardContent></CardContent>
+          <Tabs defaultValue="added">
+            <CardHeader className="items-center">
+              <TabsList>
+                <TabsTrigger value="added">Tillagda</TabsTrigger>
+                <TabsTrigger value="add">Lägg till</TabsTrigger>
+              </TabsList>
+            </CardHeader>
+            <CardContent>
+              <TabsContent value="added">Tillagda</TabsContent>
+              <TabsContent value="add">Nya</TabsContent>
+            </CardContent>
+          </Tabs>
         </Card>
       </Suspense>
     </View>
