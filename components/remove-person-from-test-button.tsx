137a1f07ecfcbeaf1be61d5b5d8dea8c227dd9f8
Elias Landin <elias.landin@me.com>
2025-03-23 20:16:39 +0100

    Add click handler, spinner and error handling

--- components/remove-person-from-test-button.tsx
+++ components/remove-person-from-test-button.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { XIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { startTransition, useActionState } from "react";
+import { removePersonFromTestAction } from "@/app/admin/actions";
+import { Spinner } from "./spinner";
 
 export const RemovePersonFromTestButton = ({
   relationId,
@@ -8,9 +13,30 @@ export const RemovePersonFromTestButton = ({
   relationId: number;
   testId: number;
 }) => {
+  const [state, formAction, isPending] = useActionState(
+    removePersonFromTestAction,
+    null,
+  );
+
+  if (state?.error) {
+    throw new Error(state.error);
+  }
+
   return (
-    <Button size="icon" variant="ghost">
-      <XIcon className="w-5 h-5" />
+    <Button
+      size="icon"
+      variant="ghost"
+      onClick={() =>
+        startTransition(() => formAction({ id: relationId, testId }))
+      }
+      title="Ta bort"
+      disabled={isPending}
+    >
+      {isPending ? (
+        <Spinner className="w-5 h-5 text-foreground" />
+      ) : (
+        <XIcon className="w-5 h-5" />
+      )}
     </Button>
   );
 };
