36ec3bc7b633951cf085823ed41afbeb653cef20
Elias Landin <elias.landin@me.com>
2025-03-23 10:38:10 +0100

    Add migration to change reference to users from auth table to profiles table

---
+++ supabase/migrations/20250323091137_change_references_to_users_from_auth_to_profiles_table.sql
@@ -0,0 +1,25 @@
+ALTER TABLE api.user_test_relations
+DROP CONSTRAINT user_test_relations_user_id_fkey;
+
+ALTER TABLE api.user_test_relations
+ADD CONSTRAINT user_test_relations_user_id_fkey
+FOREIGN KEY (user_id) REFERENCES api.profiles(id)
+ON DELETE CASCADE;
+
+
+ALTER TABLE api.city_user_relations
+DROP CONSTRAINT city_user_relations_user_id_fkey;
+
+ALTER TABLE api.city_user_relations
+ADD CONSTRAINT city_user_relations_user_id_fkey
+FOREIGN KEY (user_id) REFERENCES api.profiles(id)
+ON DELETE CASCADE;
+
+
+ALTER TABLE api.role_user_relations
+DROP CONSTRAINT role_user_relations_user_id_fkey;
+
+ALTER TABLE api.role_user_relations
+ADD CONSTRAINT role_user_relations_user_id_fkey
+FOREIGN KEY (user_id) REFERENCES api.profiles(id)
+ON DELETE CASCADE;
