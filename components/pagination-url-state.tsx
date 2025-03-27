2ac849b5e12e38821931379d2372c4897305ab89
Elias Landin <elias.landin@me.com>
2025-03-27 20:05:04 +0100

    Make previous, next buttons put page number in url

--- components/pagination-url-state.tsx
+++ components/pagination-url-state.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
 import {
   Pagination,
   PaginationContent,
@@ -17,14 +20,37 @@ export const PaginationUrlState = ({
     return;
   }
 
+  const pathname = usePathname();
+  const readOnlyParams = useSearchParams();
+
+  const currentPage = Number(readOnlyParams.get(queryKey)) || 1;
+  const searchParams = new URLSearchParams(readOnlyParams);
+  searchParams.delete(queryKey);
+  const newParams = Object.fromEntries(searchParams);
+
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious href={"#"} />
+          <PaginationPrevious
+            href={{
+              pathname,
+              query: {
+                ...newParams,
+                ...(currentPage - 1 > 1 && {
+                  [queryKey]: currentPage - 1 > 1 && currentPage - 1,
+                }),
+              },
+            }}
+          />
         </PaginationItem>
         <PaginationItem>
-          <PaginationNext href={"#"} />
+          <PaginationNext
+            href={{
+              pathname,
+              query: { ...newParams, [queryKey]: currentPage + 1 },
+            }}
+          />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
