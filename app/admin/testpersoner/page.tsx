9af54d9822021b690018a23ae34321e8edd82f36
Elias Landin <elias.landin@me.com>
2025-03-27 21:49:08 +0100

    Add pagination

--- app/admin/testpersoner/page.tsx
+++ app/admin/testpersoner/page.tsx
@@ -1,9 +1,23 @@
 import { List } from "@/components/list";
+import { PaginationUrlState } from "@/components/pagination-url-state";
 import { Card, CardContent } from "@/components/ui/card";
+import { View } from "@/components/view";
 import { getTestPersons } from "@/lib/fetchers";
 
-export default async function Page() {
-  const { data, error } = await getTestPersons();
+const PAGINATION_PAGE_SIZE = 20;
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: Promise<{
+    p?: string;
+  }>;
+}) {
+  const search = await searchParams;
+  const { data, count, error } = await getTestPersons(undefined, {
+    page: Math.max(1, Number(search?.p) || 1),
+    pageSize: PAGINATION_PAGE_SIZE,
+  });
 
   if (error) {
     return (
@@ -17,13 +31,20 @@ export default async function Page() {
   return (
     <Card>
       <CardContent>
-        <List>
-          {data.map((user) => (
-            <li key={user.id} className="border rounded-md p-4 bg-background">
-              {user.name}
-            </li>
-          ))}
-        </List>
+        <View className="gap-3">
+          <List>
+            {data.map((user) => (
+              <li key={user.id} className="border rounded-md p-4 bg-background">
+                {user.name}
+              </li>
+            ))}
+          </List>
+          <PaginationUrlState
+            itemCount={count}
+            pageSize={PAGINATION_PAGE_SIZE}
+            queryKey={"p"}
+          />
+        </View>
       </CardContent>
     </Card>
   );
