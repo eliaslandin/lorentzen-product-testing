600f359c28817e3cb38e62e2ebdb56cf9ed2651c
Elias Landin <elias.landin@me.com>
2025-04-03 21:21:22 +0200

    Add migrations to fix auth custom hook if user has no roles

---
+++ supabase/migrations/20250403190851_fix_2_custom_access_token_auth_hook.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION api.custom_access_token_hook(event jsonb)
+RETURNS jsonb
+LANGUAGE plpgsql
+STABLE
+AS $$
+  DECLARE
+    claims jsonb;
+    users_roles VARCHAR(255) [];
+  BEGIN
+    SELECT array_agg(role) INTO users_roles FROM api.role_user_relations WHERE user_id = (event->>'user_id')::UUID;
+
+    claims := event->'claims';
+
+    -- Add app_metadata object if absent
+    IF jsonb_typeof(claims->'app_metadata') IS NULL THEN
+      claims := jsonb_set(claims, '{app_metadata}', '{}');
+    END IF;
+
+    IF users_roles IS NOT NULL THEN
+      claims := jsonb_set(claims, '{app_metadata, user_roles}', to_jsonb(users_roles));
+    ELSE
+      claims := jsonb_set(claims, '{app_metadata, user_roles}', 'null'::jsonb);
+    END IF;
+
+    event := jsonb_set(event, '{claims}', claims);
+
+    RETURN event;
+  END;
+$$;
