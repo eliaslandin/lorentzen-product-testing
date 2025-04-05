9ed58f6a25a58743ea0b9e90980329854948347d
Elias Landin <elias.landin@me.com>
2025-04-05 17:59:18 +0200

    Add card for invantar bekraftning page

--- app/invanta-bekraftning/page.tsx
+++ app/invanta-bekraftning/page.tsx
@@ -1,3 +1,38 @@
+import { H1 } from "@/components/H1";
+import { P } from "@/components/P";
+import { Spinner } from "@/components/spinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { View } from "@/components/view";
+
 export default function Page() {
-  return <div>Inväntar godkännande av testhandledare...</div>;
+  return (
+    <View className="bg-muted min-h-screen justify-center items-center">
+      <Card className="max-w-3xl">
+        <CardHeader className="items-center text-center">
+          <Spinner className="w-10 h-10 text-primary my-6" />
+          <CardTitle>
+            <H1>Inväntar bekräftning från testhandledare...</H1>
+          </CardTitle>
+          <CardDescription>
+            <P>
+              Berätta din bekräftelsekod för din handledare för att bli
+              inloggad.
+            </P>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center py-6">
+          <P>Din bekräftelsekod:</P>
+          <span className="text-[200px] font-bold text-primary leading-none">
+            37
+          </span>
+        </CardContent>
+      </Card>
+    </View>
+  );
 }
