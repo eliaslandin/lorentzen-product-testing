eb53ba603f550b40a269ab2c2079add4d5916cf3
Elias Landin <elias.landin@me.com>
2025-04-06 12:58:57 +0200

    Add sign out button

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import { signOutAction } from "../actions";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -22,6 +24,7 @@ export default async function ProtectedPage() {
           user
         </div>
       </div>
+      <Button onClick={signOutAction}>Logga ut</Button>
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
