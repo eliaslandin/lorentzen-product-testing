ad745f74e732a8393be27012552e44cc84d4c694
Elias Landin <elias.landin@me.com>
2025-05-18 20:59:49 +0200

    Add form and input fields

--- components/personal-info-form.tsx
+++ components/personal-info-form.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { addPersonalInfoAction } from "@/app/actions";
+import { addPersonalInfoSchema } from "@/lib/schemas";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { useActionState } from "react";
+import { FormContent } from "./form-content";
+import { FormField } from "./form-field";
+import { Input } from "./ui/input";
+import { View } from "./view";
+import { P } from "./P";
+
 export const PersonalInfoForm = ({
   userId,
   testId,
@@ -7,9 +18,106 @@ export const PersonalInfoForm = ({
   userId: string;
   testId: number;
 }) => {
+  const [lastResult, formAction, pending] = useActionState(
+    addPersonalInfoAction,
+    undefined,
+  );
+
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: addPersonalInfoSchema,
+      });
+    },
+    shouldRevalidate: "onInput",
+    shouldValidate: "onBlur",
+  });
+
   return (
-    <div>
-      User: {userId}. Test: {testId}.
-    </div>
+    <form id={form.id} onSubmit={form.onSubmit} action={formAction}>
+      <FormContent>
+        <FormField
+          label="Epost"
+          inputId={fields.email.id}
+          errorMessage={fields.email.errors}
+        >
+          <Input
+            type="email"
+            id={fields.email.id}
+            key={fields.email.key}
+            defaultValue={fields.email.initialValue}
+          />
+        </FormField>
+        <FormField
+          label="Telefon"
+          inputId={fields.tel.id}
+          errorMessage={fields.tel.errors}
+        >
+          <Input
+            type="tel"
+            id={fields.tel.id}
+            key={fields.tel.key}
+            defaultValue={fields.tel.initialValue}
+          />
+        </FormField>
+        <FormField
+          label="Adress"
+          inputId={fields.address.id}
+          errorMessage={fields.address.errors}
+        >
+          <Input
+            type="text"
+            id={fields.address.id}
+            key={fields.address.key}
+            defaultValue={fields.address.initialValue}
+          />
+        </FormField>
+        <FormField
+          label="Postnummer"
+          inputId={fields.postal_code.id}
+          errorMessage={fields.postal_code.errors}
+        >
+          <Input
+            type="text"
+            id={fields.postal_code.id}
+            key={fields.postal_code.key}
+            defaultValue={fields.postal_code.initialValue}
+          />
+        </FormField>
+        <FormField
+          label="Användarvillkor"
+          inputId={fields.terms_accepted.id}
+          errorMessage={fields.terms_accepted.errors}
+        >
+          <View className="flex-row gap-4 items-center">
+            <P>Jag godkänner användarvillkoren:</P>
+            <Input
+              type="checkbox"
+              id={fields.terms_accepted.id}
+              key={fields.terms_accepted.key}
+              defaultChecked={fields.terms_accepted.initialValue === "true"}
+            />
+          </View>
+        </FormField>
+        <FormField
+          label="Integritetspolicy"
+          inputId={fields.privacy_policy_accepted.id}
+          errorMessage={fields.privacy_policy_accepted.errors}
+        >
+          <View className="flex-row gap-4 items-center">
+            <P>Jag godkänner integritetspolicyn:</P>
+            <Input
+              type="checkbox"
+              id={fields.privacy_policy_accepted.id}
+              key={fields.privacy_policy_accepted.key}
+              defaultChecked={
+                fields.privacy_policy_accepted.initialValue === "true"
+              }
+            />
+          </View>
+        </FormField>
+      </FormContent>
+    </form>
   );
 };
