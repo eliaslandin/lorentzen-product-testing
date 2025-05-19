7d39be7b0545d4e73189db9d10ae48fc78774bb7
Elias Landin <elias.landin@me.com>
2025-05-19 10:36:25 +0200

    Add unlock test button

--- app/admin/tester/[id]/page.tsx
+++ app/admin/tester/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { LoadingCard } from "@/components/loading-card";
 import { ParticipantsSection } from "@/components/participants-section";
 import { TestInfoSection } from "@/components/test-info-section";
+import { UnlockTestButton } from "@/components/unlock-test-button";
 import { View } from "@/components/view";
 import { Suspense } from "react";
+import { getTest } from "@/lib/fetchers";
 
 export default async function Page({
   params,
@@ -18,12 +20,19 @@ export default async function Page({
 }) {
   const { id } = await params;
   const search = await searchParams;
+  const { data: test, error } = await getTest(id);
+
+  if (error) {
+    console.error(`Couldn't get test. Error: ${JSON.stringify(error)}`);
+    throw new Error("Servererror");
+  }
 
   return (
     <View className="gap-4">
       <Suspense fallback={<LoadingCard />}>
         <TestInfoSection id={id} />
       </Suspense>
+      <UnlockTestButton testId={test.id} testIsActive={test.active} />
       <View>
         <ParticipantsSection id={id} searchParams={search} />
       </View>
---
+++ components/unlock-test-button.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+export const UnlockTestButton = ({
+  testId,
+  testIsActive,
+}: {
+  testId: number;
+  testIsActive: boolean;
+}) => {
+  return <Button>Öppna för deltagare</Button>;
+};
