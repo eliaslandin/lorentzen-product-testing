663d62f16c2325fe270d9996e0531da20647fec3
Elias Landin <elias.landin@me.com>
2025-05-22 10:15:41 +0200

    Add logo to tester layout

--- app/tester/layout.tsx
+++ app/tester/layout.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from "react";
 import { View } from "@/components/view";
+import { Logo } from "@/components/logo";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <View className="min-h-screen items-center bg-muted md:px-4 md:py-4">
-      <View className="items-center max-w-6xl">{children}</View>
+    <View className="min-h-screen items-center bg-muted md:px-4 py-4">
+      <View className="items-center max-w-6xl gap-4">
+        <header className="w-full flex justify-between px-4">
+          <Logo className="max-w-12" />
+        </header>
+        {children}
+      </View>
     </View>
   );
 }
