7076b909c0a197fa308e28b4f23237d37227dadd
Elias Landin <elias.landin@me.com>
2025-06-15 19:21:26 +0200

    Add migration for function to check if person is in a test

---
+++ supabase/migrations/20250615165345_add_user_is_in_test_fn.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION api.user_is_in_test(p_test_id BIGINT)
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN EXISTS (
+    SELECT 1
+    FROM api.test_user_relations
+    WHERE test_id = p_test_id AND user_id = ( SELECT auth.uid() )
+  );
+END;
+$$;
