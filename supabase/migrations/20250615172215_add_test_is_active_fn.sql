0de648c14a862c8f2444c50f597abc1ec4251a11
Elias Landin <elias.landin@me.com>
2025-06-15 19:30:51 +0200

    Add migration for test is active function

---
+++ supabase/migrations/20250615172215_add_test_is_active_fn.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION test_is_active(test_id BIGINT)
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+  RETURN EXISTS (
+    SELECT 1
+    FROM api.tests
+    WHERE id = test_id AND active = TRUE;
+  );
+END;
+$$;
