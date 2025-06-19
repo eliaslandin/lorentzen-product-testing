a05dc0580b5dec61af3a613bd6617bc08c186987
Elias Landin <elias.landin@me.com>
2025-06-19 09:12:03 +0200

    Add products rls tests for helper funtions and fix users not part of test not seeing products test

--- supabase/tests/products_rls.test_test.sql
+++ supabase/tests/products_rls.test_test.sql
@@ -15,9 +15,11 @@ UPDATE api.tests
 SET active = TRUE
 WHERE id = 1;
 
--- Make User 1 part of Test 1
+-- Make User 1 part of Test 1 and 2
 INSERT INTO api.user_test_relations (user_id, test_id)
-VALUES ('123e4567-e89b-12d3-a456-426614174000', 1);
+VALUES 
+	('123e4567-e89b-12d3-a456-426614174000', 1)
+	('123e4567-e89b-12d3-a456-426614174000', 2);
 
 INSERT INTO api.products (name, description, test_id) 
 VALUES	
@@ -30,23 +32,47 @@ SET LOCAL ROLE authenticated;
 SET LOCAL request.jwt.claim.sub = '123e4567-e89b-12d3-a456-426614174000';
 
 SELECT results_eq(
-	$$SELECT COUNT(*) FROM api.products$$,
+	api.test_is_active(1),
+	TRUE,
+	'Test 1 should be active'
+);
+
+SELECT results_eq(
+	api.test_is_active(2),
+	FALSE,
+	'Test 2 should be inactive'
+);
+
+SELECT results_eq(
+	api.user_is_in_test(1),
+	TRUE,
+	'User 1 should be part of Test 1'
+);
+
+SELECT results_eq(
+	api.user_is_in_test(3),
+	FALSE,
+	'User 1 should not be part of Test 3'
+);
+
+SELECT results_eq(
+	$$ SELECT COUNT(*) FROM api.products $$,
 	ARRAY[2::BIGINT],
-	'Users part of active test can view test products'
+	'Users part of active test should be able to view only active tests products'
 );
 
 SELECT throws_ok(
-	$$INSERT INTO api.products (name, test_id) VALUES ('User Created Product', 1)$$,
-	'Users can not create products'
+	$$ INSERT INTO api.products (name, test_id) VALUES ('User Created Product', 1) $$,
+	'Users should not be able to create products'
 );
 
 -- as User 2
 SET LOCAL request.jwt.claim.sub = '987fcdeb-51a2-43d7-9012-345678901234';
 
-SELECT results_ne(
-	$$SELECT COUNT(*) FROM api.products$$,
-	ARRAY[2::BIGINT],
-	'Users not part of active test cant view test products'
+SELECT results_eq(
+	$$ SELECT COUNT(*) FROM api.products $$,
+	ARRAY[0],
+	'Users not part of active test should not be able to view test products'
 );
 
 
