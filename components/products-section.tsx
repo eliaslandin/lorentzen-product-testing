301e9e14cca1085587a8b3e014019fc71db793fb
Elias Landin <elias.landin@me.com>
2025-06-30 19:32:37 +0200

    Add product image with fallback

--- components/products-section.tsx
+++ components/products-section.tsx
@@ -1,5 +1,7 @@
 import { H2 } from "./H2";
 import { List } from "./list";
+import { P } from "./P";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { View } from "./view";
@@ -7,7 +9,7 @@ import { View } from "./view";
 export const ProductsSection = () => {
   return (
     <View>
-      <H2>Produkter</H2>
+      <H2 className="pl-4">Produkter</H2>
       <Card>
         <CardHeader>
           <Button variant="secondary">Ny produkt</Button>
@@ -15,11 +17,15 @@ export const ProductsSection = () => {
         <CardContent>
           <List>
             <li>
-              <Card className="border">
-                <CardHeader>Produkt ett</CardHeader>
-                <CardContent>
-                  hdasjdh ahsdkjhas dashjdkhsa dhsajkdhsa dsahjkdhas
-                </CardContent>
+              <Card className="flex width-full border border-secondary flex-row gap-5 p-3">
+                <Avatar className="rounded-sm self-center h-32 w-32">
+                  <AvatarImage />
+                  <AvatarFallback className="rounded-none bg-secondary" />
+                </Avatar>
+                <View className="py-2">
+                  <h3 className="text-lg">Produkt ett</h3>
+                  <P>hdasjdh ahsdkjhas dashjdkhsa dhsajkdhsa dsahjkdhas</P>
+                </View>
               </Card>
             </li>
           </List>
