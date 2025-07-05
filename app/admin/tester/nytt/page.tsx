a112840ce85e03ac5c6ec3a8aeb8089fd59ab65e
Elias Landin <elias.landin@me.com>
2025-07-05 17:56:14 +0200

    Small fix

--- app/admin/tester/nytt/page.tsx
+++ app/admin/tester/nytt/page.tsx
@@ -2,7 +2,7 @@ import { View } from "@/components/view";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { CreateTestForm } from "@/components/create-test-form";
 
-export default function AddUserPage() {
+export default function Page() {
   return (
     <View className="items-center">
       <Card className="max-w-2xl">
