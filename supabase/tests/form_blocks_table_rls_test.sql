7690136b5afeb37d62b1b145ab32914f18b8b24c
Elias Landin <elias.landin@me.com>
2025-07-06 18:38:33 +0200

    Add rls test for form_blocks table

--- supabase/tests/form_blocks_table_rls_test.sql
+++ supabase/tests/form_blocks_table_rls_test.sql
@@ -16,18 +16,23 @@ OVERRIDING SYSTEM VALUE
 VALUES	(1, 'Test 1', TRUE),
 	(2, 'Test 2', FALSE);
 
+INSERT INTO api.products (id, name, test_id) 
+OVERRIDING SYSTEM VALUE
+VALUES	(1, 'Product 1', 1),
+	(2, 'Product 2', 2);
+
 -- Make User 1 part of Test 1 and 2
 INSERT INTO api.user_test_relations (user_id, test_id)
 VALUES
 ('123e4567-e89b-12d3-a456-426614174000', 1),
 ('123e4567-e89b-12d3-a456-426614174000', 2);
 
-INSERT INTO api.questions (id, title, test_id)
+INSERT INTO api.form_blocks (id, type, test_id, product_id, position, data)
 OVERRIDING SYSTEM VALUE
 VALUES
-(1, 'Test 1 Question 1', 1),
-(2, 'Test 1 Question 2', 1),
-(3, 'Test 2 Question 1', 2);
+(1, 'heading', 1, 1, 1, '{"text": "Heading 1 Test 1"}'),
+(2, 'heading', 1, 1, 2, '{"text": "Heading 2 Test 1"}'),
+(3, 'heading', 2, 2, 1, '{"text": "Heading 1 Test 2"}');
 
 -- as User 1
 SET LOCAL ROLE authenticated;
@@ -35,7 +40,7 @@ SET LOCAL request.jwt.claim.sub = '123e4567-e89b-12d3-a456-426614174000';
 
 -- Users can't select if test is inactive
 SELECT results_eq(
-	$$ SELECT COUNT(*) FROM api.questions WHERE test_id = 2 $$,
+	$$ SELECT COUNT(*) FROM api.form_blocks WHERE test_id = 2 $$,
 	ARRAY[0::BIGINT],
 	'Shouldnt be able to select if test is inactive'
 	);
@@ -43,16 +48,16 @@ SELECT results_eq(
 -- Users can't insert
 SELECT throws_ok(
 	$$
-	INSERT INTO api.questions(title)
-	VALUES ('User created question')
+	INSERT INTO api.form_blocks(type, test_id, product_id, position, data)
+	VALUES ('heading', 1, 1, 4, '{"text": "User created"}')
 	$$
 	);
 
 -- Users can't update
 SELECT results_ne(
 	$$
-	UPDATE api.questions
-	SET title = 'User updated'
+	UPDATE api.form_blocks
+	SET data = '{"text": "User created"}'
 	WHERE id = 1
 	RETURNING 1
 	$$,
@@ -63,7 +68,7 @@ SELECT results_ne(
 -- Users can't delete
 SELECT results_ne(
 	$$
-	DELETE FROM api.questions
+	DELETE FROM api.form_blocks
 	WHERE id = 1
 	RETURNING 1
 	$$,
@@ -71,11 +76,11 @@ SELECT results_ne(
 	'Shouldnt be able to delete'
 	);
 
--- Users can view questions if part of test and test is active
+-- Users can view form_blocks if part of test and test is active
 SELECT results_eq(
-	$$ SELECT COUNT(*) FROM api.questions WHERE test_id = 1 $$,
+	$$ SELECT COUNT(*) FROM api.form_blocks WHERE test_id = 1 $$,
 	ARRAY[2::BIGINT],
-	'Should be able to see questions if part of test and test is active'
+	'Should be able to see form_blocks if part of test and test is active'
 	);
 
 
@@ -84,7 +89,7 @@ SET LOCAL request.jwt.claim.sub = '987fcdeb-51a2-43d7-9012-345678901234';
 
 -- Users can't select if not part of test
 SELECT results_eq(
-	$$ SELECT COUNT(*) FROM api.questions WHERE test_id = 1 $$,
+	$$ SELECT COUNT(*) FROM api.form_blocks WHERE test_id = 1 $$,
 	ARRAY[0::BIGINT],
 	'Shouldnt be able to select if not part of test'
 	);
